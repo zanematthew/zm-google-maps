d4bc2eb8407ec321d44c655971186681f1dbc10d
Zane M. Kolnik <zanematthew@gmail.com>
2012-11-18 00:12:31 -0500

    new way of doing meta fields

--- mini-map.php
+++ mini-map.php
@@ -9,15 +9,15 @@
 
 // else its a tracks post type
 if ( $_POST['post_type'] == 'events' ) {
-    $track_id = Events::getTrackId( $_POST['post_id'] );
+    $venues_id = Events::getVenueId( $_POST['post_id'] );
 } else {
-    $track_id = $_POST['post_id'];
+    $venues_id = $_POST['post_id'];
 }
 
 ?>
 <div class="map-container">
     <div id="mini_map_target" style="height: 240px; width: 300px; border: 1px solid #ddd;"></div>
     <!-- <div id="directionsPanel"></div> -->
-    <span id="track_city" style="display: none;"><?php print Venues::getMetaField( 'city', $track_id ) ?></span>
-    <span id="track_region" style="display: none;"><?php print Venues::getMetaField( 'state', $track_id ) ?></span>
+    <span id="track_city" style="display: none;"><?php print Venues::getAttribute( array( 'key' => 'city', 'venues_id' => $venues_id ) ); ?></span>
+    <span id="track_region" style="display: none;"><?php print Venues::getAttribute( array( 'key' => 'state', 'venues_id' => $venues_id ) ); ?></span>
 </div>
